2fe3863741956c4890dcfa756aaf62db500c4d0f
Luis Miguel da Costa Pereira <luis.mdc.pereira@hotmail.com>
2012-12-23 19:59:04 +0000

    Exercicio 2 - FederatedMVC

---
+++ FederatedAuthenticationMVC/FederatedWebsite/Backup/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.IdentityModel.Claims;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var ident = User as IClaimsPrincipal;
+            var emailClaim = ident.Identities[0].Claims.FirstOrDefault(c => c.ClaimType == ClaimTypes.Role);
+            return "Hi there " + (emailClaim != null ? emailClaim.Value : "stranger");
+        }
+    }
+}
---
+++ FederatedAuthenticationMVC/FederatedWebsite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Microsoft.IdentityModel.Claims;
+using MvcApplication.CustomAttributes;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [RequireTokenAuthenticationAttribute]
+        [RequireClaims(ClaimTypes.NameIdentifier)]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Error(string message)
+        {
+            return View("Error", message);
+        }
+    }
+}
